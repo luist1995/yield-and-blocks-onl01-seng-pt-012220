d6ec2f93ffa66645b9cf853a507e0af9fb51bfab
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-02 09:05:31 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+  if block_given?
   i = 0
   
   while i < array.length
@@ -7,7 +8,10 @@ def hello_t(array)
     end
     array
   end
-  
+  puts "Hey! No block was given!"
+end
+end
+
   hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
